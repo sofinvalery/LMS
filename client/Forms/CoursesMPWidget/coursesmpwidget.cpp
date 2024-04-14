f93c9bf3ca20918eeef7ceaf4239943f97f3ae64
kit020304 <vpiksel@gmail.com>
2024-04-14 22:03:17 +0300

    fixed scroll

--- client/Forms/CoursesMPWidget/coursesmpwidget.cpp
+++ client/Forms/CoursesMPWidget/coursesmpwidget.cpp
@@ -62,7 +62,7 @@ CoursesMPWidget::CoursesMPWidget(QWidget *parent) :
 
     //ui->scrollArea->resize(this->frameGeometry().width()-20,this->frameGeometry().height()-100);
     //ui->scrollAreaWidgetContents->setMinimumWidth(this->frameGeometry().width()-20); - ширина области в которой работает скролл
-    ui->scrollAreaWidgetContents->setMinimumHeight((arrlen/columncnt)*(2*vertspace+widgetheight)+widgetheight/2);//+350);
+    ui->scrollAreaWidgetContents->setMinimumHeight((arrlen/columncnt)*(2*vertspace+widgetheight)+widgetheight/2 + 20);//+350);
     // ui->gridLayoutWidget->setFixedWidth(this->frameGeometry().width());
     // ui->gridLayoutWidget->setFixedWidth(this->frameGeometry().height());
     ui->gridLayoutWidget->resize(this->frameGeometry().width(),this->frameGeometry().height());
