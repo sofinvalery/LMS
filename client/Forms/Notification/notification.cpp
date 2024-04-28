bf2d0bdc979aa89060af4c7b4128f1da7b0dafbf
zhchqa <e.kolyada5@gmail.com>
2024-04-28 18:30:55 +0300

    notification 7.8

--- client/Forms/Notification/notification.cpp
+++ client/Forms/Notification/notification.cpp
@@ -7,25 +7,56 @@ Notification::Notification(QWidget *parent, QString notification, QString colour
     , ui(new Ui::Notification)
 {
     ui->setupUi(this);
+    //startEnterAnimation();
+    this->move(StyleManager::GetInstance()->getScreenWidth() - 290, 100); //StyleManager::GetInstance()->getScreenWidth() - 290
     StyleManager::GetInstance()->setLabelStyle(ui->notificationLabel, notification, true, colour, true, 16);
 
     ui->groupBox->setStyleSheet(
         "QGroupBox {"
         "border: 2px solid lightgrey;"
         "}");
-    QPropertyAnimation* animation = new QPropertyAnimation(ui->progressBar, "value");
-    animation->setStartValue(100);
-    animation->setEndValue(0);
-    animation->setDuration(5000);
-    animation->start();
-    connect(animation, &QPropertyAnimation::finished, this, &Notification::closeNotification);
-
-    this->move(StyleManager::GetInstance()->getScreenWidth() - 290, 100);
+    QTimer* timer = new QTimer(this);
+    timer->setInterval(5000);
+    timer->start();
+    connect(timer, &QTimer::timeout, this, &Notification::startExitAnimation);
 }
 
-void Notification::closeNotification()
+void Notification::startExitAnimation()
 {
-    this->close();
+    QPropertyAnimation* moveLeftAnimation = new QPropertyAnimation(this, "geometry");
+    moveLeftAnimation->setStartValue(geometry());
+    moveLeftAnimation->setEndValue(QRect(x() - 30, y(), width(), height()));
+    moveLeftAnimation->setDuration(300);
+
+    QPropertyAnimation* moveOutAnimation = new QPropertyAnimation(this, "geometry");
+    moveOutAnimation->setStartValue(geometry());
+    moveOutAnimation->setEndValue(QRect(StyleManager::GetInstance()->getScreenWidth(), y(), width(), height()));
+    moveOutAnimation->setDuration(300);
+
+    connect(moveLeftAnimation, &QPropertyAnimation::finished, [moveOutAnimation]() {
+        moveOutAnimation->start();
+    });
+    connect(moveOutAnimation, &QPropertyAnimation::finished, this, &QWidget::close);
+    moveLeftAnimation->start();
+}
+
+void Notification::startEnterAnimation()
+{
+    // this->move(StyleManager::GetInstance()->getScreenWidth(), 100); //StyleManager::GetInstance()->getScreenWidth() - 290
+    // QPropertyAnimation* moveLeftAnimation = new QPropertyAnimation(this, "geometry");
+    // moveLeftAnimation->setStartValue(geometry());
+    // moveLeftAnimation->setEndValue(QRect(x() - 320, y(), width(), height()));
+    // moveLeftAnimation->setDuration(300);
+
+    // QPropertyAnimation* moveRightAnimation = new QPropertyAnimation(this, "geometry");
+    // moveRightAnimation->setStartValue(geometry());
+    // moveRightAnimation->setEndValue(QRect(x() + 30, y(), width(), height()));
+    // moveRightAnimation->setDuration(300);
+
+    // connect(moveLeftAnimation, &QPropertyAnimation::finished, [moveRightAnimation]() {
+    //     moveRightAnimation->start();
+    // });
+    // moveLeftAnimation->start();
 }
 
 Notification::~Notification()
