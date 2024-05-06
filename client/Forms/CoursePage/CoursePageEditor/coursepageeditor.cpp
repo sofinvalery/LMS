dad7ad670128fa4c1203cb300b958ea4ec4de6eb
kit <vpiksel@gmail.com>
2024-05-06 14:09:24 +0300

    course editor dz adding

--- client/Forms/CoursePage/CoursePageEditor/coursepageeditor.cpp
+++ client/Forms/CoursePage/CoursePageEditor/coursepageeditor.cpp
@@ -11,9 +11,14 @@ CoursePageEditor::CoursePageEditor(CoursePage * coursepage, QWidget *parent)
     this->coursepage = coursepage;
     //StyleManager::GetInstance()->setLineEditStyle(ui->ComponentOrderLineEdit, "Порядок компонента", false, 16, 300, 30);
     StyleManager::GetInstance()->setLabelStyle(ui->ComponentOrderLabel, "Номер компонента по порядку:", false, "black", true, 16);
+    StyleManager::GetInstance()->setLabelStyle(ui->MaxMarkLabel, "Максимум баллов за задание:", false, "black", true, 16);
+    StyleManager::GetInstance()->setLabelStyle(ui->TaskContentLabel, "Описание задания:", false, "black", true, 16);
     ui->ComponentOrderLabel->setFixedSize(ui->ComponentOrderLabel->sizeHint().width(), ui->ComponentOrderLabel->sizeHint().height());
+    ui->ComponentOrderLabel->setFixedSize(ui->MaxMarkLabel->sizeHint().width(), ui->MaxMarkLabel->sizeHint().height());
+    ui->ComponentOrderLabel->setFixedSize(ui->MaxMarkLabel->sizeHint().width(), ui->TaskContentLabel->sizeHint().height());
     StyleManager::GetInstance()->setLineEditStyle(ui->NameOnComponentLineEdit, "Название компонента", false, 16, 300, 30);
-    ui->TutorialTextEdit->hide();
+    StyleManager::GetInstance()->setLineEditStyle(ui->AllowedTypeOfFilesLineEdit, "Разрешенные типы файлов", false, 16, 300, 30);
+    ui->ContentTextEdit->hide();
     ui->NameOnComponentLineEdit->hide();
     ui->ComponentOrderSpinBox->hide();
     ui->ComponentOrderLabel->hide();
@@ -24,6 +29,10 @@ CoursePageEditor::CoursePageEditor(CoursePage * coursepage, QWidget *parent)
     ui->LoadFileButton->hide();
     ui->PathLabel1->hide();
     ui->PathLabel2->hide();
+    ui->AllowedTypeOfFilesLineEdit->hide();
+    ui->MaxMarkLabel->hide();
+    ui->MaxMarkSpinBox->hide();
+    ui->TaskContentLabel->hide();
 }
 
 CoursePageEditor::~CoursePageEditor()
@@ -63,7 +72,7 @@ void CoursePageEditor::on_AddWidgetButton_clicked()
         ui->AddTextButton->hide();
 
         AddingStatus = 0;
-        ui->TutorialTextEdit->hide();
+        ui->ContentTextEdit->hide();
         ui->NameOnComponentLineEdit->hide();
         ui->ComponentOrderSpinBox->hide();
         ui->ComponentOrderLabel->hide();
@@ -120,22 +129,22 @@ void CoursePageEditor::on_DoneButton_clicked()
 {
     if(AddingStatus == 1){                                  // добавить на сервер тест (не доделано)
         coursepage->CleanComponents();
-        coursepage->GetCourse()->AddCourseComponent(new CourseTest(12312312, ui->ComponentOrderSpinBox->value(), ui->NameOnComponentLineEdit->text(), 10, "", 1000, 9, "сдал", QDate(2004,4,4)), ui->ComponentOrderSpinBox->value()-1);
+        coursepage->GetCourse()->AddCourseComponent(new CourseTest(-100, ui->ComponentOrderSpinBox->value(), ui->NameOnComponentLineEdit->text(), 10, "", 1000, 9, "сдал", QDate(2004,4,4)), ui->ComponentOrderSpinBox->value()-1);
         coursepage->ShowComponents();
     }
     if(AddingStatus == 2){                                  // добавить на сервер загрузочный файл
         coursepage->CleanComponents();
-        coursepage->GetCourse()->AddCourseComponent(new CoursePdf(22222, ui->ComponentOrderSpinBox->value(), ui->NameOnComponentLineEdit->text(),  ui->PathLabel2->text()), ui->ComponentOrderSpinBox->value()-1);
+        coursepage->GetCourse()->AddCourseComponent(new CoursePdf(-100, ui->ComponentOrderSpinBox->value(), ui->NameOnComponentLineEdit->text(),  ui->PathLabel2->text()), ui->ComponentOrderSpinBox->value()-1);
         coursepage->ShowComponents();
     }
     if (AddingStatus == 3){                                  // добавить на сервер дз (не доделано)
         coursepage->CleanComponents();
-        coursepage->GetCourse()->AddCourseComponent(new CourseTask(123123, ui->ComponentOrderSpinBox->value(), "", 100, 123, "", "", QDate(2004,4,4), 9, "", ui->NameOnComponentLineEdit->text()), ui->ComponentOrderSpinBox->value()-1);
+        coursepage->GetCourse()->AddCourseComponent(new CourseTask(-100, ui->ComponentOrderSpinBox->value(), ui->ContentTextEdit->toPlainText(), ui->MaxMarkSpinBox->value(), 7000, ui->AllowedTypeOfFilesLineEdit->text(), "", QDate(2004,4,4), 0, "", ui->NameOnComponentLineEdit->text()), ui->ComponentOrderSpinBox->value()-1);
         coursepage->ShowComponents();
     }
     if(AddingStatus == 4){                                  // добавить на сервер туториал
         coursepage->CleanComponents();
-        coursepage->GetCourse()->AddCourseComponent(new CourseTutorials(44444, ui->ComponentOrderSpinBox->value(), ui->TutorialTextEdit->toPlainText()), ui->ComponentOrderSpinBox->value()-1);
+        coursepage->GetCourse()->AddCourseComponent(new CourseTutorials(-100, ui->ComponentOrderSpinBox->value(), ui->ContentTextEdit->toPlainText()), ui->ComponentOrderSpinBox->value()-1);
         coursepage->ShowComponents();
     }
     if (DeletingStatus == 1){                               // удалить из сервера элемент  ui->ComponentOrderSpinBox->value()-1
@@ -178,9 +187,9 @@ void CoursePageEditor::on_AddFileButton_clicked()
 
 void CoursePageEditor::on_AddTextButton_clicked()
 {
-    if (ui->TutorialTextEdit->isHidden()){
+    if (ui->ContentTextEdit->isHidden()){
         AddingStatus = 4;
-        ui->TutorialTextEdit->show();
+        ui->ContentTextEdit->show();
         ui->ComponentOrderLabel->show();
         ui->ComponentOrderSpinBox->show();
         ui->AddDzButton->setEnabled(false);
@@ -189,7 +198,8 @@ void CoursePageEditor::on_AddTextButton_clicked()
     }
     else{
         AddingStatus = 0;
-        ui->TutorialTextEdit->hide();
+        ui->ContentTextEdit->clear();
+        ui->ContentTextEdit->hide();
         ui->ComponentOrderLabel->hide();
         ui->ComponentOrderSpinBox->hide();
         ui->AddDzButton->setEnabled(true);
@@ -230,6 +240,11 @@ void CoursePageEditor::on_AddDzButton_clicked()
         ui->ComponentOrderLabel->show();
         ui->ComponentOrderSpinBox->show();
         ui->NameOnComponentLineEdit->show();
+        ui->ContentTextEdit->show();
+        ui->AllowedTypeOfFilesLineEdit->show();
+        ui->MaxMarkLabel->show();
+        ui->MaxMarkSpinBox->show();
+        ui->TaskContentLabel->show();
         ui->AddTextButton->setEnabled(false);
         ui->AddTestButton->setEnabled(false);
         ui->AddFileButton->setEnabled(false);
@@ -237,9 +252,15 @@ void CoursePageEditor::on_AddDzButton_clicked()
     else
     {
         AddingStatus = 0;
+        ui->ContentTextEdit->clear();
         ui->ComponentOrderLabel->hide();
         ui->ComponentOrderSpinBox->hide();
+        ui->ContentTextEdit->hide();
         ui->NameOnComponentLineEdit->hide();
+        ui->AllowedTypeOfFilesLineEdit->hide();
+        ui->MaxMarkLabel->hide();
+        ui->MaxMarkSpinBox->hide();
+        ui->TaskContentLabel->hide();
         ui->AddTextButton->setEnabled(true);
         ui->AddTestButton->setEnabled(true);
         ui->AddFileButton->setEnabled(true);
