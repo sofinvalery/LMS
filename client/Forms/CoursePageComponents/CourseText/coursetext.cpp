60dfa7a395386fc7549379da168c39616af9080a
Gleb <korotaevgv@gmail.com>
2024-05-08 00:57:12 +0300

    fix last commit

--- client/Forms/CoursePageComponents/CourseText/coursetext.cpp
+++ client/Forms/CoursePageComponents/CourseText/coursetext.cpp
@@ -9,8 +9,6 @@ CourseText::CourseText(CourseComponent * text, QWidget *parent)
     ui->setupUi(this);
     this->text = qobject_cast<CourseTutorials*>(text);
     StyleManager::GetInstance()->setLabelStyle(ui->label, this->text->getContent(), false, "black", true, 18);
-    ui->label->setFixedSize(ui->label->sizeHint().width(), ui->label->sizeHint().height());
-    ui->label->setAlignment(Qt::AlignBottom);
     ui->label->setWordWrap(true);
     ui->label->setAlignment(Qt::AlignTop | Qt::AlignLeft);
     ui->label->setMaximumWidth(StyleManager::GetInstance()->getScreenWidth()-50);
